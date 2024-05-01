b30489075d111638ecec0e0d056282e9d58f89b9
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2024-05-01 11:55:26 -0300

    Agregando componente Loading

---
+++ challenge-dev/src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import "../../App.css";
+
+function Loading(){
+    return (
+        <div className="container-loader">
+            <div className="loader"></div>
+            <p className="message">Loading...</p>            
+        </div>
+    )
+}
+
+export default Loading;
